5f6da71b82fc7a13389f3c2dcdff7b376a49771e
RicardoFMelo <ricardomarquesmelo@gmail.com>
2025-07-30 21:38:20 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,25 +1,39 @@
+import userData from '../fixtures/users/userData.json'
+
 describe('Orange HRM Tests', () => {
   const selectorList = {
-    usernameField:"[name='username']",
-    passwordField:"[name='password']",
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".oxd-text--h6",
     wrongCredentialAlert: ".oxd-alert-content"
-
-
   }
+
+  const userDataLocal = {
+    userSuccess: {
+      username: 'Admin',
+      password: 'admin123'
+    },
+    userFail: {
+      username: 'Test',
+      password: 'Test'
+    }
+  }
+
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Admin')
-    cy.get(selectorList.passwordField).type('admin123')
+    cy.get(selectorList.usernameField, { timeout: 7000 }).type(userDataLocal.userSuccess.username)
+    cy.get(selectorList.passwordField, { timeout: 7000 }).type(userDataLocal.userSuccess.password)
     cy.get(selectorList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorList.dashboardGrid, { timeout: 7000 })
   })
+
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorList.usernameField).type('Test')
-    cy.get(selectorList.passwordField).type('Test')
+    cy.get(selectorList.usernameField).type(userDataLocal.userFail.username)
+    cy.get(selectorList.passwordField).type(userDataLocal.userFail.password)
     cy.get(selectorList.loginButton).click()
     cy.get(selectorList.wrongCredentialAlert)
   })
